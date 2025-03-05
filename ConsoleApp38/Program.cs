f9374c247d095e69ad4ff0924bda6b984f238c9e
amarshendage <amarshendage3@gmail.com>
2025-03-05 12:21:39 +0530

    this is new assignment

---
+++ ConsoleApp38/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp38
+{
+    internal class Program
+    {
+        static void Main()
+        {
+                
+                Console.WriteLine("Enter height in inches: ");
+                double heightInInches = Convert.ToDouble(Console.ReadLine());
+
+                
+                double heightInCm = heightInInches * 2.54;
+
+                
+                string category;
+                if (heightInCm < 150)
+                {
+                    category = "Dwarf";
+                }
+                else if (heightInCm >= 150 && heightInCm < 165)
+                {
+                    category = "Average height";
+                }
+                else if (heightInCm >= 165 && heightInCm < 195)
+                {
+                    category = "Tall";
+                }
+                else
+                {
+                    category = "Abnormal height";
+                }
+
+                
+                Console.WriteLine("Height category: " + category);
+            
+        }
+
+
+
+
+
+
+
+    }
+}
+}
