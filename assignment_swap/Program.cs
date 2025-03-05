2f4413765b9890721d4fbb5c3ad73d91c362788d
amarshendage <amarshendage3@gmail.com>
2025-03-05 14:10:47 +0530

    this is swapping

---
+++ assignment_swap/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace assignment_swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = 10 , b = 15;
+            Console.WriteLine("before swap"+"a = " + a + " b = " + b);
+            //a = a + b;
+            //b = a - b;
+            //a = a - b;
+
+            //a = a ^ b;
+            //b = a ^ b;
+            //a = a ^ b;
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("after sawp"+"a = " + a + " b = " + b);
+            Console.ReadLine();
+        }
+    }
+}
