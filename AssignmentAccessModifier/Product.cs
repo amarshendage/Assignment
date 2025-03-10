c64866d2d6031b05efa81cfeff8d4cdee1bd52cf
amarshendage <amarshendage3@gmail.com>
2025-03-10 12:17:58 +0530

    this is acccess modifier

---
+++ AssignmentAccessModifier/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentAccessModifier
+{
+    class Product
+    {
+        public int productId = 10;
+        private string productName = "abc";
+
+        public void test()
+        {
+            int a = 10;
+            Console.WriteLine(productName);
+        }
+        protected void test1()
+        {
+            test();
+            Console.WriteLine();
+        }
+    }
+   
+}
---
+++ AssignmentAccessModifier/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AssignmentAccessModifier
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Product productobject = new Product();
+            Console.WriteLine(productobject.productId);
+
+            productobject.test();
+        }
+    }
+}
