55cef784806ec3f5f21eb20bff8261ceab4edb66
amarshendage <amarshendage3@gmail.com>
2025-03-18 15:06:53 +0530

    inheritance

---
+++ assigemnentinherritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using static assigemnentinherritance.employee;
+
+namespace assigemnentinherritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.empId = 10;
+            e1.empName = "Nisha";
+            e1.location = "Pune";
+            System.Console.WriteLine("Object of parent class Employee : ");
+            System.Console.WriteLine(e1.empName);
+            System.Console.WriteLine(e1.empId);
+            System.Console.WriteLine(e1.location);
+
+
+
+            Manager m1 = new Manager();
+            m1.empId = 10;
+            m1.empName = "Nisha";
+            m1.location = "Pune";
+            m1.department = "Accounting";
+            System.Console.WriteLine("Object of parent class Manager : ");
+            System.Console.WriteLine(m1.empName);
+            System.Console.WriteLine(m1.empId);
+            System.Console.WriteLine(m1.location);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assigemnentinherritance/employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigemnentinherritance
+{
+    class employee
+    {
+        public class Employee
+        {
+            public string empName { get; set; }
+            public int empId { get; set; }
+            public string location { get; set; }
+        }
+
+
+
+        public class Manager : Employee
+        {
+
+            public string department { get; set; }
+
+            public long getSalesOfYear()
+            {
+                return 1000;
+            }
+        }
+
+        public class Salesman : Employee
+        {
+            public string region { get; set; }
+
+            public long GetSalesOfTheCurrentMonth()
+            {
+                return 100;
+            }
+
+        }
+    }
+}
